20ac5f8f7bb6138a23cc4f14f6a04925c56ced3b
Kanepiro <hiro-k@hh.iij4u.or.jp>
2026-06-19 06:12:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="lyric-tapper-v10-kampe-above-button";
+const CACHE="lyric-tapper-v11-lifted-button-ui";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
